56935dcc9ca17e7c118f2318973613db2cc15600
CoudaCode <couda.dm@gmail.com>
2023-09-10 22:46:30 +0000

    ok

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { config } from "dotenv";
+import { connectDB } from "./config/db.js";
+import { inProduction } from "./config/env.js";
+import path from "path";
+import cors from "cors";
+import { fileURLToPath } from "url";
+
+const app = express();
+config({
+  path: path.join(process.cwd(), ".env.local"),
+});
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("Image"));
+app.use("/Image", express.static(path.join(__dirname, "Image")));
+// app.use(express.static(path.join(__dirname, "../client/dist")));
+// if (inProduction) {
+//   app.get("/*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+//   });
+// }
+
+connectDB()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  })
+  .catch((e) => {
+    console.log(
+      "An orror has occured while connecting to mongodb : ",
+      e.message
+    );
+  });
+
+
